e8d35f6a56bb2f6d62dca12d92e2cfabb8488141
Jackson <jzambranogarzon@gmail.com>
2024-02-29 22:20:38 -0500

    class/shopify-api: add fetch of products

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,18 @@
-export const MainProducts = () => {
+
+const getProducts = async () => {
+  const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+    headers: new Headers({
+      "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || ""
+    })
+  })
+
+  const data = await response.json()
+  return data
+}
+
+export const MainProducts = async () => {
+  const product = await getProducts()
+
   return (
     <section>
       <h1>Main products</h1>
