f13a41146ced4e12a9650fed19bee3be8e33bedd
Jackson <jzambranogarzon@gmail.com>
2024-03-30 18:03:24 -0500

    class/backend-for-frontend: apply layer to bring the products

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -3,13 +3,14 @@ import styles from './MainProducts.module.sass'
 import { getProducts } from "app/services/shopify"
 
 export const MainProducts = async () => {
-  const { products } = await getProducts()
+  const response = await fetch('http://localhost:3000/api')
+  const { products } = await response.json()
 
   return (
     <section className={styles.MainProducts}>
       <h3>New products released!</h3>
       <div className={styles.MainProducts__grid}>
-        {products?.map(product => {
+        {products?.map((product: any) => {
           const imageSrc = product.images[0].src
           return (
             <article key={product.id}>
