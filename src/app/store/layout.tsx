c6d4235e0ac55e11068fb4d1b1173931c2307f0b
Jackson <jzambranogarzon@gmail.com>
2024-04-01 21:49:57 -0500

    class/filtered-products: add filter in products

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,15 +1,17 @@
 import { getCollections } from "app/services/shopify/collections"
 import Link from "next/link"
+import styles from './StoreLayout.module.sass'
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const collections = await getCollections()
 
   return (
-    <main>
-      <nav>
+    <main className={styles.StoreLayout}>
+      <h1>Explore</h1>
+      <nav className={styles.StoreLayout__list}>
         {
           collections.map((collection: any) => (
-            <Link key={collection.id} href={'/store/'+collection.handle}>
+            <Link key={collection.id} href={'/store/'+collection.handle} className={styles.StoreLayout__chip}>
               {collection.title}
             </Link>
           ))
--- src/services/shopify/collections.ts
+++ src/services/shopify/collections.ts
@@ -1,6 +1,12 @@
 import { env } from "app/config/env";
 import { shopifyUrls } from "./urls";
 
+export type CollectionType = {
+  id: string,
+  title: string,
+  handle: string
+}
+
 export const getCollections = async () => {
   try {
     const response = await fetch(shopifyUrls.collections.all, {
@@ -10,14 +16,30 @@ export const getCollections = async () => {
     })
 
     const { smart_collections } = await response.json()
-    const transformedColelctions = smart_collections.map((collection: any) => {
+    const transformedCollections: CollectionType[] = smart_collections.map((collection: any) => {
       return {
         id: collection.id,
         title: collection.title,
         handle: collection.handle
       }
     })
-    return transformedColelctions 
+    return transformedCollections 
+  } catch (error) {
+    console.log('error >>',error);
+    return []
+  }
+}
+
+export const getCollectionsProducts = async (id: string) => {
+  try {
+    const response = await fetch(shopifyUrls.collections.products(id), {
+      headers: new Headers({
+        "X-Shopify-Access-Token": env.SHOPIFY_TOKEN
+      })
+    })
+
+    const { products=[] } = await response.json() || { }
+    return products
   } catch (error) {
     console.log('error >>',error);
   }
