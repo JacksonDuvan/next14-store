a31ca6064c61aa89797f8d833e3a2eb7186a754b
Jackson <jzambranogarzon@gmail.com>
2024-04-10 22:28:30 -0500

    class/product-view: create product view and cache of main products

--- src/services/shopify/products.ts
+++ src/services/shopify/products.ts
@@ -1,17 +1,55 @@
 import { env } from "app/config/env";
 import { shopifyUrls } from "./urls";
 
-export const getProducts = async () => {
+export const getProducts = async (id?: string): Promise<ProductType[]> => {
   try {
-    const response = await fetch(shopifyUrls.products.all, {
+    const apiUrl = id ? `${shopifyUrls.products.all}?ids=${id}` : shopifyUrls.products.all
+    const response = await fetch(apiUrl, {
       headers: new Headers({
         "X-Shopify-Access-Token": env.SHOPIFY_TOKEN
       })
     })
 
-    const data = await response.json()
-    return data || {} 
+    const { products } = await response.json() || {}
+
+    const transformedProducts = products.map((product: any) => {
+      return {
+        id: product.id,
+        gql_id: product.variants[0].admin_graphql_api_id,
+        title: product.title,
+        description: product.body_html,
+        price: product.variants[0].price,
+        image: product.images[0].src,
+        quantity: product.variants[0].inventory_quantity,
+        handle: product.handle,
+        tags: product.tags,
+      }
+    })
+
+    return transformedProducts 
   } catch (error) {
     console.log('error >>',error);
+    return []
+  }
+}
+
+export const getMainProducts = async () => {
+  try {
+    const response = await fetch(shopifyUrls.products.mainProducts, {
+      headers: new Headers({
+        'X-Shopify-Access-Token': env.SHOPIFY_TOKEN
+      }),
+      cache: 'no-cache'
+      // next: {
+      //   revalidate: 10
+      // }
+    })
+  
+    const {products} = await response.json()
+  
+    return products
+  } catch (error) {
+    console.log('error >>',error);
+    return []
   }
 }
