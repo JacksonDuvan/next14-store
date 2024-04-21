30374123b9a64826bec95414eef5b53fd21412d9
Jackson <jzambranogarzon@gmail.com>
2024-04-20 22:25:25 -0500

    class/graphql: add graphql the shopify

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,20 @@
 "use server"
 
-export const handleCreateUser = (formData: any) => {
-  console.log('handleCreateUser >>', formData);
+import { GraphQLClientSingleton } from "app/graphql"
+import { createUserMutation } from "app/graphql/mutations/createUserMutation"
+
+export const handleCreateUser = async (formData: any) => {
+  const formDataObject = Object.fromEntries(formData)
+  console.log('formDataObject >>',formDataObject);
+  delete formDataObject["password_confirmation"]
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+  const variables = {
+    input: {
+      ...formDataObject,
+      phone: '+57'+ formDataObject.phone
+    }
+  }
+
+  const data = await graphqlClient.request(createUserMutation, variables)
+  console.log('data >>',data);
 }
