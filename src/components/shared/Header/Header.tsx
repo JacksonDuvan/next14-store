98346edce0c6cbb9558cf16bb797960a7e8bc299
Jackson <jzambranogarzon@gmail.com>
2024-04-23 22:03:00 -0500

    class/validation-token: add validation accessToken

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,10 +1,9 @@
 import Link from 'next/link'
 import styles from './Header.module.css'
-import { cookies } from 'next/headers'
+import { validateAccessToken, ICustomerName } from 'app/utils/auth/validateAccessToken'
 
-export const Header = () => {
-  const cookiesStore = cookies()
-  const token = cookiesStore.get('accessToken')?.value
+export const Header = async () => {
+  const customer = await validateAccessToken()
 
   return (
     <header>
@@ -18,7 +17,7 @@ export const Header = () => {
           </Link>
         </ul>
       </nav>
-      {token ? (<p>Hola!</p>) : (<Link href="/login">Login</Link>)}
+      {customer?.firstName ? (<p>Hola! {customer?.firstName}</p>) : (<Link href="/login">Login</Link>)}
     </header>
   )
 }
