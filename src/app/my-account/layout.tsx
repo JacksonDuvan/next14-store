16c0b838d98afabf3d76d2f540dad6dfcb138594
Jackson <jzambranogarzon@gmail.com>
2024-05-15 19:58:59 -0500

    class/parallel-routing: learning parallel routing

---
+++ src/app/my-account/layout.tsx
@@ -0,0 +1,17 @@
+
+interface MyAccountLayoutProps {
+  children: React.ReactNode,
+  ordersInfo?: React.ReactNode,
+  userInfo?: React.ReactNode
+}
+
+export default async function MyAccountLayout({ children, userInfo, ordersInfo }: MyAccountLayoutProps){
+
+  return (
+    <div>
+     {children}
+     {userInfo}
+     {ordersInfo}
+    </div>
+  )
+}
---
+++ src/app/my-account/page.tsx
@@ -0,0 +1,9 @@
+
+export default async function MyAccountPage(){
+
+  return (
+    <div>
+     <h1>My Account</h1>
+    </div>
+  )
+}
