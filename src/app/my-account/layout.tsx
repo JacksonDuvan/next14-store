750ae1e276858bd1f89c743849e8ede1308b251e
Jackson <jzambranogarzon@gmail.com>
2024-05-18 15:57:11 -0500

    fixed-errors: fixed errors

--- src/app/my-account/layout.tsx
+++ src/app/my-account/layout.tsx
@@ -1,8 +1,8 @@
 
 interface MyAccountLayoutProps {
   children: React.ReactNode,
-  ordersInfo?: React.ReactNode,
-  userInfo?: React.ReactNode
+  ordersInfo: React.ReactNode,
+  userInfo: React.ReactNode
 }
 
 export default async function MyAccountLayout({ children, userInfo, ordersInfo }: MyAccountLayoutProps){
--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -1,4 +1,6 @@
 
+export const dynamic = 'force-dynamic'
+
 export default async function MyAccountPage(){
 
   return (
